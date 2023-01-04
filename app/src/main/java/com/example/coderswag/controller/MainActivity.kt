cc62cf5852b4a4a927d6c075e6fd8ca0a7beeced
Gustavo <ruiz.gustavo97@gmail.com>
2023-01-03 21:07:04 -0800

    created listView along with custom layout

--- app/src/main/java/com/example/coderswag/controller/MainActivity.kt
+++ app/src/main/java/com/example/coderswag/controller/MainActivity.kt
@@ -2,11 +2,25 @@ package com.example.coderswag.controller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
 import com.example.coderswag.R
+import com.example.coderswag.model.Category
+import com.example.coderswag.services.DataService
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var adapter: ArrayAdapter<Category>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        adapter = ArrayAdapter(this,
+        android.R.layout.simple_list_item_1,
+        DataService.categories)
+
+        val categoryListView: ListView = findViewById(R.id.categoryListView)
+        categoryListView.adapter = adapter
     }
 }
